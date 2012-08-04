2e074690ca6e5e0711958c302347bd4b8fb1d9e3
Gabriel Dunne <gdunne@quilime.com>
2012-08-03 17:47:58 -0700

    Added Mongoose to handle the data with Mongo DB

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
-var express = require("express");
+var express  = require("express"),
+    mongoose = require("mongoose")
 
+require("./datamodel").configureSchema(mongoose.Schema, mongoose);
+var Shader = mongoose.model("Shader");
 
 var app = express.createServer(express.logger());
+app.db = mongoose.connect(process.env.MONGO_URI);
 app.configure(function () {
   app.use(express.static(__dirname + "/public"));
   app.use(express.bodyParser());
@@ -23,6 +27,93 @@ app.configure("production", function () {
 
 app.get("/", function (req, res) {
   res.render("index", { env: app.settings.env, layout: false });
+});
+
+
+app.get("/get", function(req, res){
+  Shader.find()
+    .skip(req.query["skip"])
+    .limit(req.query["limit"])
+    .sort("date", -1)
+    .exec(function (err, shaders) {
+      if (err) {
+        res.send("Error fetching shaders.");
+      } else {
+        res.json({
+          status : "OK",
+          shaders : shaders
+        });
+      }
+    });
+});
+
+
+app.get("/count", function(req, res){
+  Shader.find().count().exec(function (err, count) {
+    if (err) {
+      res.send("Error counting shaders.");
+    } else {
+      res.json({
+        status : "OK",
+        count : count
+      });
+    }
+  });
+});
+
+
+app.get("/short/:short_id",function(req, res){
+  Shader.findOne({ short_id : req.params.short_id }, function(err, shader){
+    if (err) {
+      res.send("Error finding shader with short_id: " + req.params.short_id);
+    } else {
+      res.json({
+        status : "OK",
+        shader : shader
+      });
+    }
+  });
+});
+
+
+app.post("/save", function(req, res){
+
+  console.log("Receiving new shader to store...");
+  console.log(req.body);
+
+  var generateID = function(length) {
+    var id = "";
+    var chars = "qwrtypsdfghjklzxcvbnm0123456789";
+    while(id.length < length) {
+      var pos = Math.floor(Math.random() * chars.length - 1);
+      id += chars.substring(pos, pos + 1);
+    }
+    return id;
+  }
+
+  var saveShader = function(iteration, callback) {
+    var newShader = new Shader({
+      short_id : generateID(iteration),
+      code_lzma : req.body.code_lzma
+    });
+    newShader.save(function (err) {
+      if (err !== null && err.code === 11000) {
+        console.log("Duplicate short_id; Generating another...");
+        saveShader(iteration += 1, callback);
+      }
+      else {
+        callback(this.emitted.complete[0]);
+      }
+    });
+  };
+
+  saveShader(1, function(savedShader) {
+    console.log("Saving Success!", savedShader);
+    res.json({
+      status : "OK",
+      shader : savedShader
+    });
+  });
 });
 
 
